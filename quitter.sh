162361b3ecc5f4eaa69394dd2d9b1824ff27d059
Hugo Martin <hugo.martin.duloz@gmail.com>
2020-12-22 23:20:33 +0100

    added function to call the right function based on first arg

--- quitter.sh
+++ quitter.sh
@@ -20,3 +20,49 @@
 CONFIG_DIR="~/.config/quitter"
 HORAIRES_FILE="horaires.db"
 PID_FILE="boucle.pid"
+
+
+##################################################################
+# Cette fonction regarde le premier paramètre pour savoir quelle #
+# fonction appeler pour réaliser l'action et renvoie 0, si le    #
+# mode n'existe pas, la fonction renvoie l'aide et une erreur (1)#
+##################################################################
+function trouver_mode()
+{
+  case "$1" in 
+    -q)
+      echo "TODO fonction arreter boucle" ;;
+    -l)
+      echo "TODO fonction lister RDV à venir" ;;
+    -a)
+      echo "TODO fonction lister tout RDV" ;;
+    -r)
+      echo "TODO supprimer fonction à l'heure / tag précisée" ;;
+    -h)
+      echo "TODO afficher aide" ;;
+    [0-9][0-9][0-9][0-9] | [0-9]-[0-9]-[0-9]-[0-9]-[0-9]-[0-9]-[0-9]-[0-9]/[0-9][0-9][0-9][0-9])
+      echo "TODO fonction ajouter RDV" ;;
+    *)
+      echo "TODO option non reconnue afficher help" ;;
+  esac
+}
+
+###################################################################
+# Cette fonction affiche le mode d'emploi du programme quitter    #
+###################################################################
+
+
+
+########################################
+# Cette fonction sert à déterminer si  #
+# nombre de paramètre est bon, sinon   #
+# le programme plante (1) et affiche   #
+# l'aide.                              #
+########################################
+
+if test "$#" -ge 1 && test "$#" -le 3
+then  
+  trouver_mode $1
+else
+  echo "Invalid number of args. help ->"
+fi
